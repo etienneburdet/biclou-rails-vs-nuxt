ab45ade2948d9f0aeb2442dfdf798ad52f048956
Etienne <etienne.burdet@gmail.com>
2019-11-23 15:39:38 +0100

    rental policy/controller/view

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,39 @@
 class RentalsController < ApplicationController
+  before_action :set_rental, only: %i[update destroy]
+  before_action :set_bike, only: %i[new create]
+
+  def index
+    @rentals = policy_scope(Rental).order(created_at: :desc)
+  end
+
+  def new
+    @rental = Rental.new
+    authorize @rental
+  end
+
+  def create
+    @rental = Rental.new(rental_params)
+    @rental.user = current_user
+    @rental.bike = @bike
+    authorize @rental
+    if @rental.save
+      redirect_to rentals_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:end_date, :start_date)
+  end
+
+  def set_rental
+    @rental = Rental.find(params[:id])
+  end
+
+  def set_bike
+    @bike = Bike.find(params[:bike_id])
+  end
 end
--- app/policies/rental_policy.rb
+++ app/policies/rental_policy.rb
@@ -1,7 +1,18 @@
 class RentalPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(user: user)
     end
+  end
+  
+  def new?
+    true
+  end
+  def create?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
   end
 end
