0e01c1a88b7ffc77b4b6cbca9e0b47668a16d82f
BURDET ETIENNE <etienne.burdet@gmail.com>
2019-12-10 11:55:19 +0100

    Add rental form

--- nuxt/pages/rentals/new.vue
+++ nuxt/pages/rentals/new.vue
@@ -1,14 +1,53 @@
 <template lang="html">
- <div class="">
-   {{ bikeId}}
- </div>
+ <b-container>
+   <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+     <b-form-group
+       id="input-group-1"
+       label="Start date"
+       label-for="input-1"
+     >
+       <b-form-input
+         id="input-1"
+         v-model="form.startDate"
+         type="date"
+         required
+         placeholder="Start date"
+       ></b-form-input>
+     </b-form-group>
+
+     <b-form-group
+       id="input-group-1"
+       label="Start date"
+       label-for="input-1"
+     >
+       <b-form-input
+         id="input-1"
+         v-model="form.endDate"
+         type="date"
+         required
+         placeholder="End date"
+       ></b-form-input>
+
+       <b-button type="submit" variant="primary">Submit</b-button>
+       <b-button type="reset" variant="danger">Reset</b-button>
+     </b-form-group>
+   </b-form>
+   <b-card class="mt-3" header="Form Data Result">
+     <pre class="m-0">{{ form }}</pre>
+   </b-card>
+ </b-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      bikeId: this.$route.query.bike_id
+      show: true,
+      form: {
+        startDate: new Date,
+        endDate: new Date,
+        bikeId: this.$route.query.bike_id,
+      }
     }
   },
   async asyncData({ $axios, params }) {
@@ -16,6 +55,29 @@ export default {
     $axios.setToken(process.env.CLOUDANT_TOKEN, 'Basic ');
     const bike = await $axios.$get(query);
     return { bike };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      this.$axios.setToken(process.env.CLOUDANT_TOKEN, 'Basic ')
+      const _id = `rent-${Date.now()}`
+      this.form._id = _id
+      const rental = JSON.stringify(this.form)
+      this.$axios.$put(_id, rental)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    },
+    onReset(evt) {
+      evt.preventDefault()
+      // Reset our form values
+      this.form.description = ''
+      this.form.name = ''
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
   }
 }
 </script>
