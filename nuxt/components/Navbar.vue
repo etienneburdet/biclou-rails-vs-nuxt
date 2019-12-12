6333e661444d97d6988dd116718fb110c3bc5b53
BURDET ETIENNE <etienne.burdet@gmail.com>
2019-12-12 22:34:29 +0100

    basic nav

--- nuxt/components/Navbar.vue
+++ nuxt/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template lang="html">
-  <b-navbar fixed="bottom">
-    <b-navbar-nav pills fixed="top" class="justify-content-center my-2 w-100">
-      <b-nav-item active>Search</b-nav-item>
-      <b-nav-item>Bikes</b-nav-item>
-      <b-nav-item>Rentals</b-nav-item>
-      <b-nav-item disabled>Login</b-nav-item>
+  <b-navbar fixed="bottom" variant="primary">
+    <b-navbar-nav class="justify-content-center my-2 w-100">
+      <b-nav-item href="/">Search</b-nav-item>
+      <b-nav-item href="/bikes">Bikes</b-nav-item>
+      <b-nav-item href="/rentals">Rentals</b-nav-item>
+      <b-nav-item href="/login">Login</b-nav-item>
     </b-navbar-nav>
   </b-navbar>
 </template>
---
+++ nuxt/pages/rentals/index.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <h1>Rentals</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
