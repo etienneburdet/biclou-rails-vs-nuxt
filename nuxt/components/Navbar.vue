5bcf35c4e585c3ad80570eec90ec35365f6ca710
BURDET ETIENNE <etienne.burdet@gmail.com>
2019-12-12 22:25:44 +0100

    position navbar

---
+++ nuxt/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template lang="html">
+  <b-navbar fixed="bottom">
+    <b-navbar-nav pills fixed="top" class="justify-content-center my-2 w-100">
+      <b-nav-item active>Search</b-nav-item>
+      <b-nav-item>Bikes</b-nav-item>
+      <b-nav-item>Rentals</b-nav-item>
+      <b-nav-item disabled>Login</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
